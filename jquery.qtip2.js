f7aecfc288df52c0d464bc4b9d3edc0ecbf9e007
jquerysp <jquerysp@outlook.com>
2014-09-30 03:31:12 +0000

    1

--- jquery.qtip2.js
+++ jquery.qtip2.js
@@ -24,6 +24,7 @@ today = dd+'/'+mm+'/'+yyyy;
 //alert(today);
 
 $("input[title$='Off Site']").change(function(){
+	alert($("input[title$='Off Site']").val());
 	if($("input[title$='Off Site']").is(':checked')){
 	alert("is checked");
         $("nobr:contains('Site')").parent('h3').parent('td').parent('tr').hide();
