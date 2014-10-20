b671c079fe5d9e03c294b8810bce5db41fd8424e
jquerysp <jquerysp@outlook.com>
2014-10-20 01:53:15 +0000

    1

--- jquery.qtip2.js
+++ jquery.qtip2.js
@@ -187,7 +187,7 @@ $("input[title$='Injured/Diseased']").change(function(){
 	if (!$("input[oldtitle$='Injured/Diseased']").is(':checked')){
 		$("select[oldtitle$='Type of First Aid']").val("");
 		$("textarea[title$='First Aid Treatment']").val("");
-		$("select[oldtitle$='Body Part - Primary']").val("");
+		$("select[title$='Body Part - Primary']").val("");
 		$("nobr:containsExactCase('Body Part - All affected')").parents("td:first").find('input:checkbox').each(function(){
 			$(this).prop('checked',false);
 		});
