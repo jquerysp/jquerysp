993c7ff9d8843a6d2931e83718d4a02a0050ac55
jquerysp@outlook.com <jquerysp@outlook.com@ba57a006-0a34-081b-3517-373100f67aee>
2015-04-16 13:41:56 +0000

    1

--- jquery.qtip2.js
+++ jquery.qtip2.js
@@ -508,7 +508,7 @@ $("nobr:contains('Immediate Manager or Investigator')").parent('h3').parent('td'
     },
     position: {
         adjust: {
-            x: 215
+            x: 400
         }
     }
 });
@@ -520,7 +520,31 @@ $("nobr:containsExactCase('Other Relevant Supervisor-Manager 1')").parent('h3').
     },
     position: {
         adjust: {
-            x: 215
+            x: 400
+        }
+    }
+});
+
+$("nobr:containsExactCase('Other Relevant Supervisor-Manager 2')").parent('h3').parent('td').parent('tr').qtip({ 
+    content: {
+      text: 'If you believe some other supervisors and managers are relevant to this and should be notified or involved in the investigation, please enter their name here. You can only Enter one name at a time.<br><br>Please do NOT enter any Sourcing Unit Director, SHE Manager, Vice President or WHS Coordinator names because they already get notifications for all incidents reported.',
+      title: 'Optional field'
+    },
+    position: {
+        adjust: {
+            x: 400
+        }
+    }
+});
+
+$("nobr:containsExactCase('Other Relevant Supervisor-Manager 3')").parent('h3').parent('td').parent('tr').qtip({ 
+    content: {
+      text: 'If you believe some other supervisors and managers are relevant to this and should be notified or involved in the investigation, please enter their name here. You can only Enter one name at a time.<br><br>Please do NOT enter any Sourcing Unit Director, SHE Manager, Vice President or WHS Coordinator names because they already get notifications for all incidents reported.',
+      title: 'Optional field'
+    },
+    position: {
+        adjust: {
+            x: 400
         }
     }
 });
