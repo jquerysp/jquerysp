860a19edde83483ff5d28c8e6528918478af31f6
jquerysp <jquerysp@outlook.com>
2015-08-17 16:27:24 +1000

    Create jquery.qtipt.js

---
+++ jquery.qtipt.js
@@ -0,0 +1,24 @@
+function PreSaveAction(){
+    var itemTitle = $("textarea[title$='Comments if not Recordable']").val();
+    if (itemTitle.length < 1) {
+            alert("Please enter Comments if not Recordable before saving it again.");
+            return false;
+    }
+};
+
+$(document).ready(function(){
+
+$("input[title$='Comments if not Recordable']").qtip({ 
+    content: {
+        text: 'Tick the checkbox when you fill out all the necessary fields above. After ticking off, you can submit this incident report by clicking the save botton at the top left or the bottom right hand side of the page.'
+    },
+    position: {
+        adjust: {
+            x: 215
+        }
+    }
+});
+
+
+
+});
